6134a1e905791e6edfa6cfdf9a25fe681e23175e
PHANINDRA <phane431@gmail.com>
2023-10-17 21:48:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
    const { email, password } = req.body;
 
-   // Simple validation
+   
    if (!email || !password) {
        return res.status(400).json({ error: 'Please provide both email and password' });
    }
